fe0a0cbe58f411e67c81dfd04cc53e15ac6f36f2
nastyenkal <docnastyadit@gmail.com>
2025-05-14 00:22:11 +0300

    started

---
+++ Statistic/src/main/java/mephi/b23902/statistic/statistic/Statistic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b23902.statistic.statistic;
+
+/**
+ *
+ * @author Настя
+ */
+public class Statistic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
